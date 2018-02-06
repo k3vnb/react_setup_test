ad532800b3bb46341f95bf6e7bdd2a921cd4b55e
Kevin Boyle <papershack@gmail.com>
2018-02-05 22:21:55 -0800

    successfully install hot module

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Ticket(){
+function Ticket(props){
   return (
     <div>
       <h3>{props.location} - {props.names}</h3>
