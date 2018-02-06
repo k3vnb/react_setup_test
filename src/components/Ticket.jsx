246417f7dbfdc5e81c4d7f053071975c83e67659
Kevin Boyle <papershack@gmail.com>
2018-02-05 21:58:23 -0800

    set up until proptypes

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Ticket(){
+  return (
+    <div>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+   );
+}
+
+export default Ticket;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(){
+
+  return (
+    <Ticket
+      location="BigTown, USA"
+      name="Hull and Scotch"
+      issue="Big fish on the line"/>
+  );
+}
+
+export default TicketList;
