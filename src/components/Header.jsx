f0e2cde6c394740c074e9550545de50c17439fe9
Kevin Boyle <papershack@gmail.com>
2018-02-06 21:25:09 -0800

    set up router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
   return(
     <div>
       <h3>Help Me</h3>
+      <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
   );
 }
