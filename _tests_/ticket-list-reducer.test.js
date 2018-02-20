8fbc53fc069c7578a9fb1cb26e5b0df1ec2d61ef
Kevin Boyle <papershack@gmail.com>
2018-02-20 08:00:10 -0800

    install jest, add to package.json, set up jest directories & files

---
+++ _tests_/ticket-list-reducer.test.js
@@ -0,0 +1,10 @@
+import ticketListReducer from './../../src/reducers/ticket-list-reducer';
+
+describe('ticketListReducer', () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+
+});
+//THIS TEST WILL FAIL
