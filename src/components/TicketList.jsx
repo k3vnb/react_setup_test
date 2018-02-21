645300055acabf7beb81839a33b57b524089f9bc
Kevin Boyle <papershack@gmail.com>
2018-02-20 21:58:59 -0800

    refactor ticketlist.jsx

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -14,8 +14,7 @@ function TicketList(props){
                   formattedWaitTime={ticket.formattedWaitTime}
                   currentRouterPath={props.currentRouterPath}
                   key={ticketId}
-                  ticketId={ticketId}
-                  onTicketSelection={props.onTicketSelection}/>
+                  ticketId={ticketId}/>
               })}
 
     </div>
@@ -25,7 +24,7 @@ function TicketList(props){
 TicketList.propTypes = {
   ticketList: PropTypes.object,
   currentRouterPath: PropTypes.string,
-  onTicketSelection: PropTypes.func
+  // onTicketSelection: PropTypes.func
 };
 
 export default TicketList;
